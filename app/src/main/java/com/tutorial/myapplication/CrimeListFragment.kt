894952a0948c778585ee1b11fd54d1376760344d
jayzedx <d.chalermpong@gmail.com>
2022-09-01 22:47:59 +0700

    Refactoring useless code

--- app/src/main/java/com/tutorial/myapplication/CrimeListFragment.kt
+++ app/src/main/java/com/tutorial/myapplication/CrimeListFragment.kt
@@ -9,37 +9,23 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tutorial.myapplication.databinding.FragmentCrimeListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CrimeListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CrimeListFragment : Fragment() {
+    private var param1: String? = null
+
     private val crimeListViewModel: CrimeListViewModel by viewModels()
-
-
     private var _binding: FragmentCrimeListBinding? = null
     private val binding
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -61,21 +47,10 @@ class CrimeListFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CrimeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CrimeListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
